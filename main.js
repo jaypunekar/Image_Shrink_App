140d74ae53f5c09191afe1694e94d34f63864657
Jay Punekar <jaypunekar01@gmail.com>
2023-05-24 01:22:26 +0530

    frontend compleated

--- main.js
+++ main.js
@@ -1,16 +1,95 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron')
+
+process.env.NODE_ENV = 'devlopment';
+isDev = process.env.NODE_ENV !== 'production' ? true : false;
+isMac = process.platform == 'darwin' ? true : false;
 
 let mainWindow;
+let aboutWindow;
+
 const createWindow = () => {
     mainWindow = new BrowserWindow({
         title: "ImageShrink",
         width: 500, 
         height: 600,
-        icon: './assets/icons/icon_256x256.png'
+        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+        resizable: isDev ? true : false
     });
 
     // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
     mainWindow.loadFile('./app/index.html')
 }
 
-app.on('ready', createWindow)
+const createAboutWindow = () => {
+    aboutWindow = new BrowserWindow({
+        title: "About ImageShrink",
+        width: 300, 
+        height: 300,
+        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+        resizable: false
+    });
+
+    // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+    aboutWindow.loadFile('./app/about.html')
+}
+
+app.on('ready', () => {
+    createWindow();
+
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+
+    globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
+    globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools())
+
+
+    mainWindow.on('closed', () => mainWindow = null);
+})
+
+const menu = [
+    ...(isMac ? [
+        {
+            label: app.name,
+            submenu: [
+                {
+                    label: "About",
+                    click: createAboutWindow,
+                }
+            ]
+        }
+    ] : []),
+    {
+        role: 'fileMenu'
+    },
+    ...(!isMac ? [
+        {
+            label: 'Help',
+            submenu: [
+                {
+                    label: "About",
+                    click: createAboutWindow,
+                }
+            ]
+        }
+    ]: []),
+    ...(isDev ? [
+        {
+            label: "Developer",
+            submenu: [
+                {role: 'reload'},
+                {role: 'forcereload'},
+                {type: 'separator'},
+                {role: 'toggleDevTools'}
+            ]
+
+        }
+    ] : [])
+]
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (!isMac) app.quit()
+})
