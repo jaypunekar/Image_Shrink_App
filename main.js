e9d944b793ffbb11510fe9f8e4454678402e8ec7
Jay Punekar <jaypunekar01@gmail.com>
2023-05-26 22:50:10 +0530

    functionality added

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const { app, BrowserWindow, Menu, globalShortcut } = require('electron')
+const path = require('path');
+const os = require('os');
+const { app, BrowserWindow, Menu, globalShortcut, ipcMain, shell } = require('electron');
+const imageminJpegtran = require ('imagemin-jpegtran');
+const imagemin = require('imagemin')
+const imageminPngquant = require('imagemin-pngquant');
+// const imageminMozjpeg = require('imagemin-mozjpeg')
+const slash = require('slash')
+
 
 process.env.NODE_ENV = 'devlopment';
 isDev = process.env.NODE_ENV !== 'production' ? true : false;
@@ -10,14 +18,48 @@ let aboutWindow;
 const createWindow = () => {
     mainWindow = new BrowserWindow({
         title: "ImageShrink",
-        width: 500, 
+        width: isDev ? 1000 : 500, 
         height: 600,
         icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-        resizable: isDev ? true : false
+        resizable: isDev ? true : false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
     });
+
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
 
     // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
     mainWindow.loadFile('./app/index.html')
+}
+
+ipcMain.on('image:minimize', (e, options) => {
+    options.dest = path.join(os.homedir(), 'imageshrink');
+    shrinkImage(options)
+});
+
+async function shrinkImage({imgPath, quality, dest}) {
+   try {
+        const pngQuality = quality / 100;
+        const files = await imagemin([slash(imgPath)], {
+            destination: dest,
+            plugins: [
+                imageminJpegtran({quality}),
+                imageminPngquant({
+                    quality: [pngQuality, pngQuality]
+                }),
+            ]
+        });
+
+        console.log(files);
+
+        shell.openPath(dest);
+   } catch (err) {
+    console.log(err)
+   }
 }
 
 const createAboutWindow = () => {
